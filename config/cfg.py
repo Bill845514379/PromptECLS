92ce0daf63a65bf0d9777d8eaad08dddfd843ab1
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-01-05 22:26:38 +0800

    Add files via upload

---
+++ config/cfg.py
@@ -0,0 +1,27 @@
+cfg = {
+    'gpu_id': 0,
+    'max_len': 220,
+    'train_batch_size': 1,
+    'test_batch_size': 32,
+    'learning_rate': 1e-5,
+    'epoch': 10,
+    'K': 8,
+    'Kt': 500,
+    # 'template': '[X1] [X2]? [MASK].',
+    'template': '[X1] ? [MASK] , [X2]',
+    'answer': ['No', 'Yes'],
+    'device': 'cuda',
+    'optimizer': 'Adam',
+    'word_size': 50265
+}
+
+hyper_roberta = {
+    'word_dim': 1024,
+    'dropout': 0.1
+}
+
+path = {
+    'neg_path': 'data/rt-polaritydata/neg_label.txt',
+    'pos_path': 'data/rt-polaritydata/pos_label.txt',
+    'roberta_path': 'roberta-large'
+}
